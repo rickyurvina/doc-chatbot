351f70d5ed3a9846d840c4cffb2acf4ccea7af9b
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-08-16 19:29:26 -0400

    add next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true, asyncWebAssembly: true, layers: true };
-    return config;
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-};
-
-export default nextConfig;
+module.exports = {
+  output: 'standalone',
+}
